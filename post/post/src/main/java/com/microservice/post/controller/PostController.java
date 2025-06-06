68fddce9a450a03f92514d243e62aae6984c2960
Shivendra Ojha <shivendraojha370@gmail.com>
2025-06-07 01:08:00 +0530

    Initial commit: Microservices project with all services

--- post/post/src/main/java/com/microservice/post/controller/PostController.java
+++ post/post/src/main/java/com/microservice/post/controller/PostController.java
@@ -3,10 +3,13 @@ package com.microservice.post.controller;
 import com.microservice.post.entity.Post;
 import com.microservice.post.payload.PostDto;
 import com.microservice.post.service.PostService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -30,8 +33,20 @@ public class PostController {
 
     //http://localhost:8081/api/posts/{postId}/comments
     @GetMapping("/{postId}/comments")
+    @CircuitBreaker(name="commentBreaker", fallbackMethod = "commentFallback")
     public ResponseEntity<PostDto> getPostWithComments(@PathVariable String postId) {
         PostDto postDto = postService.getPostWithComments(postId);
         return new ResponseEntity<>(postDto, HttpStatus.OK);
     }
+
+    public ResponseEntity<PostDto> commentFallback(String postId, Throwable throwable) {
+        PostDto fallbackDto = new PostDto();
+        fallbackDto.setPostId(postId);
+        fallbackDto.setTitle("Fallback Title");
+        fallbackDto.setDescription("Comment Service is down. Showing post only.");
+        fallbackDto.setComments(List.of()); // return empty comment list
+
+        return new ResponseEntity<>(fallbackDto, HttpStatus.OK);
+    }
+
 }
